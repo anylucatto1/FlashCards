c357e6169925466fac3b4f0c0f55e39c8042ed76
anylucatto1 <105176912+anylucatto1@users.noreply.github.com>
2024-11-26 09:10:51 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'História',
+    'Como surgiu o drink Cuba Libre?',
+    'A bebida surgiu após a independência de Cuba, como colônia espanhola, com a ajuda dos EUA.'
+)
+
+criaCartao(
+    'Literatura',
+    'No livro "Toca das Raposas", quem é o pai de Kevin Day?',
+    'O pai dele é o treinador David Waymack.'
+)
+
+criaCartao(
+    'Geek',
+    'Em Duna, qual o nome do predestinado, através de uma profecia, a ser líder do planeta Arrakis e dos fremen?',
+    'O nome do personagem é Paul Atreides.'
+)
+
+criaCartao(
+    'Literatura',
+    'Quantos livros possuem a saga Trono de Vidro?',
+    'Com os extras, a saga possui 8 livros.'
+)
